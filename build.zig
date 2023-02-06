a1acea7062f43fbb4cae76e37d27a96e5b5c9513
jackji <jichengde@gmail.com>
2023-02-06 16:08:45 +0800

    adapt to zig master

--- build.zig
+++ build.zig
@@ -1,22 +1,19 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
-    const mode = b.standardReleaseOptions();
-
-    const main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
-
+pub fn build(b: *std.Build) void {
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+    });
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
 }
 
-pub fn getPkg() std.build.Pkg {
-    return .{
-        .name = "zasync",
-        .source = .{
+pub fn getPkg(b: *std.Build) *std.Build.Module {
+    return b.createModule(.{
+        .source_file = .{
             .path = comptime thisDir() ++ "/src/main.zig",
         },
-    };
+    });
 }
 
 fn thisDir() []const u8 {
