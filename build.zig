b126c77418c099d781cb766423875c5f59daad04
Daniel Gomes Vargas <dgv@fastmail.com>
2024-05-28 22:19:59 -0300

    added lib build

--- build.zig
+++ build.zig
@@ -1,6 +1,14 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const lib = b.addStaticLibrary(.{
+        .name = "async",
+        .root_source_file = b.path("src/main.zig"),
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
+    });
+    b.installArtifact(lib);
+
     const main_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
     });
